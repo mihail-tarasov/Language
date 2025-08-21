8c28418d2cc40839b8f1b68a35ceb14e7550c978
Mihail Tarasov <mihail.tarasov7@yandex.ru>
2025-08-21 21:54:30 +0300

    first commit

---
+++ src/main/java/ru/mihail/tarasov7/Language/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package ru.mihail.tarasov7.Language.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String homePage(Model model){
+        model.addAttribute("title","Это страничка с твоими карточками!");
+        return "homePage";
+    }
+}
---
+++ src/test/java/ru/mihail/tarasov7/Language/LanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mihail.tarasov7.Language;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

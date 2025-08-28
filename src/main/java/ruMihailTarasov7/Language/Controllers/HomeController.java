3d0cf54f90811b43b771142c4e281f96a40d09c1
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-08-29 01:20:54 +0300

    проверенная версия

--- src/main/java/ruMihailTarasov7/Language/Controllers/HomeController.java
+++ src/main/java/ruMihailTarasov7/Language/Controllers/HomeController.java
@@ -1,14 +1,20 @@
 package ruMihailTarasov7.Language.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ruMihailTarasov7.Language.Models.Post;
+import ruMihailTarasov7.Language.Repository.PostRepository;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private PostRepository postRepository;
     @GetMapping("/home")
     public String homePage(Model model){
-        model.addAttribute("title","Домашняя станица");
+        Iterable<Post> posts= postRepository.findAll();
+        model.addAttribute("posts",posts);
         return "homePage";
     }
 }

516c8b65acd6495a1f7e179506a3df78dbb13841
Mihail Tarasov <mihail.tarasov7@yandex.ru>
2025-09-18 22:38:30 +0300

    Добавил папку security, в папке добавил класс securityconfig.В папке models добавил класс user

---
+++ src/main/java/ruMihailTarasov7/Language/Security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package ruMihailTarasov7.Language.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}

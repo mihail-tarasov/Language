98ca42c9eef173d51440b1aca2bb07a46bc2fe8d
Mihail Tarasov <mihail.tarasov7@yandex.ru>
2025-09-18 22:57:35 +0300

    коммит особо без изменений для пуша

--- src/main/java/ruMihailTarasov7/Language/Security/SecurityConfig.java
+++ src/main/java/ruMihailTarasov7/Language/Security/SecurityConfig.java
@@ -2,13 +2,25 @@ package ruMihailTarasov7.Language.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ruMihailTarasov7.Language.Models.User;
+import ruMihailTarasov7.Language.Repository.UserRepository;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            User user=userRepository.findByUsername(username);
+            if(user!=null) return user;
+            throw new UsernameNotFoundException("User '"+username+"' not found");
+        };
     }
 }
